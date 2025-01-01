ff3d3f38b511d2931d9ca372a054e1d7b0d3cc27
safi <safiasad12@gmail.com>
2025-01-01 17:57:07 +0530

    [safi] ADD: implemented notes validation

--- src/routes/note.routes.ts
+++ src/routes/note.routes.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import userAuth from '../middlewares/auth.middleware';
 import { create, getNote,getUserNotes,updateNote,deletePermanently } from '../controllers/note.controller'
+import { validateNote, validateUpdateNote } from "../validation/note.validation";
 
 const noteRouter = express.Router();
 
-noteRouter.post('/', userAuth, create);
+noteRouter.post('/', validateNote, userAuth, create);
 noteRouter.get('/', userAuth, getUserNotes);
 noteRouter.get('/:id', userAuth, getNote);
-noteRouter.put('/:id', userAuth, updateNote);
+noteRouter.put('/:id', userAuth, validateUpdateNote, updateNote);
 noteRouter.delete('/:id', userAuth, deletePermanently);
 
 export default noteRouter;
